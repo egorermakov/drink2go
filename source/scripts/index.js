74223703741aef4b378e0e785aef8800f3f9d266
Egor Ermakov <im.egorermakov@gmail.com>
2025-02-13 06:49:21 +0300

    Добавляет остальные устройства и моб. меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.header__nav ');
+const navToggle = document.querySelector('.header__toggle');
+
+navMain.classList.remove('header__nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('header__nav--closed')) {
+    navMain.classList.remove('header__nav--closed');
+    navMain.classList.add('header__nav--opened');
+  } else {
+    navMain.classList.add('header__nav--closed');
+    navMain.classList.remove('header__nav--opened');
+  }
+});
