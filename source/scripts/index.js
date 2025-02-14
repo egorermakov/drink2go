93c300914da926bcb542bfb583df90326dd62827
Egor Ermakov <im.egorermakov@gmail.com>
2025-02-14 03:44:35 +0300

    Добавляет swiper

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,14 +1,26 @@
-const navMain = document.querySelector('.header__nav ');
+const navMain = document.querySelector('.header__nav');
 const navToggle = document.querySelector('.header__toggle');
 
 navMain.classList.remove('header__nav--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('header__nav--closed')) {
-    navMain.classList.remove('header__nav--closed');
-    navMain.classList.add('header__nav--opened');
-  } else {
-    navMain.classList.add('header__nav--closed');
-    navMain.classList.remove('header__nav--opened');
-  }
+navToggle.addEventListener('click', () => {
+  navMain.classList.toggle('header__nav--closed');
+  navMain.classList.toggle('header__nav--opened');
+});
+
+const promoSlider = new Swiper('.promo__slider', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+    bulletClass: 'slider__bullet',
+    bulletActiveClass: 'slider__bullet--active',
+  },
+
+  navigation: {
+    nextEl: '.slider__button--next',
+    prevEl: '.slider__button--prev',
+  },
 });
