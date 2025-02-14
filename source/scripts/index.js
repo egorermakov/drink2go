de6f8e101f31b405fb46de128c504e0fc34a7a0c
Egor Ermakov <im.egorermakov@gmail.com>
2025-02-14 05:15:11 +0300

    Добавляет leaflet

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -24,3 +24,20 @@ const promoSlider = new Swiper('.promo__slider', {
     prevEl: '.slider__button--prev',
   },
 });
+
+var map = L.map('map').setView([59.968142, 30.316425], 17);
+
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var mapPin = L.icon({
+  iconUrl: '../images/map-pin.svg',
+
+  iconSize:     [38, 50],
+  iconAnchor:   [-75, 75],
+  popupAnchor:  [-3, -76]
+});
+
+L.marker([59.968142, 30.316425], {icon: mapPin}).addTo(map);
